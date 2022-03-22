e1b1c01124a009e2e152c299f80354be00925e3f
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-22 19:42:41 +0100

    Add create method to category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,35 @@
 class CategoriesController < ApplicationController
 
   def index
-
     @categories = Category.find_by(user_id: current_user.id)
   end
+
+  def create
+    @categories = current_user.categories.create!(
+      name: params[:category][:name],
+      icon: params[:category][:icon]
+    )
+    respond_to do |format|
+      if @categories.save
+        format.html { redirect_to  user_categories_path, notice: 'Category was created successfully' }
+        format.json { render :show, status: :created, location: @categories }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @categories.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  
+  private
+
+  def set_category
+    @categories = Category.find(params[:id])
+  end
+
+  def category_params
+    params.permit(:name, :icon)
+  end
+  
   
 end
