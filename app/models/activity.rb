389544fc779bcbcb645f38707f9a35eccd536940
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-24 08:32:30 +0100

    Add validation to models

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,5 +3,5 @@ class Activity < ApplicationRecord
   has_many :activities_categories
   has_and_belongs_to_many :categories
   
-  validates_presence_of :name
+  validates_presence_of :name, :amount
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   belongs_to :user, dependent: :destroy
   has_many :activities_categories
   has_and_belongs_to_many :activities
-  validates_presence_of :name
+  validates_presence_of :name, :icon
 
   def total_amount
     activities.sum(:amount)
